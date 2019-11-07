1b0bfefa408b9b91b8db885c605b81ea6140b8a8
Brent Groves <brent.groves@gmail.com>
2019-11-07 17:00:03 +0000

    first commit

--- p2-13318.sh
+++ p2-13318.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#( node /home/brent/srcnode/Plex13318/index & node /home/brent/srcnode/MySql13318 )  > /dev/null 2>&1 
-( node /home/brent/srcnode/Plex13318/index & node /home/brent/srcnode/MySql13318 & ) > /dev/null 3>&1
+( node /home/brent/srcnode/Plex13318/index & node /home/brent/srcnode/MySql13318 )  > /dev/null 2>&1 
+#( node /home/brent/srcnode/Plex13318/index & node /home/brent/srcnode/MySql13318 & ) > /dev/null 3>&1
 
 
+#( node ~/srcnode/Plex13318/index & node ~/srcnode/MySql13318 )  > /dev/null 2>&1 
+( node ~/srcnode/Plex13318/index & node ~/srcnode/MySql13318 )
