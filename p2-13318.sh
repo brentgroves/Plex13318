b409aacf5d5ebba06fa1ab4eaa161f5325cec22b
Brent Groves <brent.groves@gmail.com>
2019-11-07 13:15:57 -0500

    Added record production

--- p2-13318.sh
+++ p2-13318.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-( node /home/brent/srcnode/Plex13318/index & node /home/brent/srcnode/MySql13318 )  > /dev/null 2>&1 
+#( node /home/brent/srcnode/Plex13318/index & node /home/brent/srcnode/MySql13318 )  > /dev/null 2>&1 
 #( node /home/brent/srcnode/Plex13318/index & node /home/brent/srcnode/MySql13318 & ) > /dev/null 3>&1
-
 
 #( node ~/srcnode/Plex13318/index & node ~/srcnode/MySql13318 )  > /dev/null 2>&1 
 ( node ~/srcnode/Plex13318/index & node ~/srcnode/MySql13318 )
